3f52aefd1cd77bb928d31d4e72291ae34e70173e
dm123321_31md <dm12332131mdgaming@gmail.com>
2024-05-18 22:48:53 -0400

    Finally a working /project page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,38 +1,124 @@
 <!-- Title -->
-<h1>My Projects</h1>
+<h1 id="title">My Projects</h1>
 
-<!-- Search Bar -->
-<input placeholder="Search Project">
+<!-- Search bar -->
+<input bind:value={query} on:input={() => {
+	// Records time
+	const start = Date.now();
 
-<!-- Project Cards -->
-<div>
-	{#each [] as project, index}
-		{project}
-	{/each}
+	// Updates search on input
+	results = projects.filter((project) => {
+
+		// Cleans data
+		const clean = query.trim().toLowerCase();
+
+		// Shows all results with empty query
+		if(clean.length === 0) return true;
+		
+		// Extracts data
+		const tags = Array.from(clean.matchAll(/#([-a-zA-Z0-9]*)/g)).map((match) => match[1]);
+		const text = clean.replace(/\s*[#@][-a-zA-Z0-9]*\s*/g, " ").trim();
+		const versions = Array.from(clean.matchAll(/@([-a-zA-Z0-9]*)/g)).map((match) => match[1]);
+
+		// Filters by tags
+		if(
+			tags.length !== 0 &&
+			!tags.some((tag) => project.tags.join(",").toLowerCase().includes(tag))
+		) return false;
+
+		// Filters by version
+		if(
+			versions.length !== 0 &&
+			!versions.some((version) => project.version.toLowerCase().includes(version))
+		) return false;
+
+		// Filters by name and description
+		if(
+			text.length !== 0 &&
+			!project.name.toLowerCase().includes(text) &&
+			!project.description.toLowerCase().includes(text)
+		) return false;
+
+		// Passes filter
+		return true;
+	});
+
+	// Updates time
+	const end = Date.now();
+	timelapse = end - start;
+}} placeholder="Search Project | @version or #tag" id="search">
+
+<!-- Projects -->
+<div id="projects">
+	{#if results.length === 0}
+		<div id="project-none">I found nothing, sorry... Q . Q</div>
+	{:else}
+		{#each results as project, index (index)}
+			<div class="project">
+				<div class="project-header">
+					<a href="projects/{project["id"]}" class="project-name">{project["name"]}</a>
+					<div class="project-version">{project["version"]}</div>
+				</div>
+				<div class="project-body">
+					<div class="project-description">{project["description"]}</div>
+					<div class="project-tags">
+						{#each project["tags"] as tag}
+							<div class="project-tag">{tag}</div>
+						{/each}
+					</div>
+				</div>
+			</div>
+		{/each}
+	{/if}
 </div>
+
+<!-- Footer -->
+<footer>
+	<div id="counter">{results.length === 3 ? ":3" : results.length} {results.length === 1 ? "result" : "results"} found</div>
+	<div id="timelapse">Search took {timelapse} ms</div>
+</footer>
 
 <!-- Script -->
 <script lang="ts">
-	// import { projects } from "$lib/projects/projects";
-	import type { Project } from "$lib/types";
+	// Defines project type
+	type Project = {
+		description: string;
+		id: string;
+		name: string;
+		tags: string[];
+		version: string;
+	};
 
-	// Imports
-	
+	// Defines projects
+	const projects: Project[] = [
+		{
+			description: "Soda.css - The minimalistic & flexible solution to CSS templates.",
+			id: "soda",
+			name: "Soda.css",
+			tags: [ "web", "css" ],
+			version: "Beta"
+		}
+	];
+
+	// Sets up search bar
+	let query: string = "";
+	let results: Project[] = projects;
+	let timelapse = 0;
 	
 </script>
 
 <!-- Style -->
 <style lang="scss">
 	// Title
-	h1 {
+	#title {
 		font-size: 25px;
 		font-weight: bold;
 		line-height: 30px;
 		text-align: center;
 	}
 
-	// Search Bar
-	input {
+	// Search bar
+	#search {
 		background-color: rgb(var(--soda-less-black));
 		border-radius: 5px;
 		margin: 30px 0px;
@@ -51,11 +137,86 @@
 		}
 	}
 
-	// Project Cards
-	div {
+	// Projects
+	#projects {
 		display: flex;
-		flex-wrap: wrap;
-		gap: 15px 15px;
-		justify-content: center;
+		flex-direction: column;
+
+		.project {
+			border-bottom: solid 1px rgb(var(--soda-white));
+			padding: 15px 25px;
+
+			&:first-child {
+				border-top: solid 1px rgb(var(--soda-white));
+			}
+
+			.project-header {
+				align-items: center;
+				display: flex;
+				gap: 0px 15px;
+				padding: 5px 0px;
+
+				.project-name {
+					font-size: 20px;
+					font-weight: bold;
+					line-height: 25px;
+					position: relative;
+
+					&::after {
+						background-color:rgb(var(--soda-white));
+						bottom: -5px;
+						content: "";
+						height: 3px;
+						left: 0%;
+						position: absolute;
+						transform: scale(0, 1);
+						transition: all 0.1s ease-in-out;
+						width: 100%;
+					}
+
+					&:hover::after {
+						transform: scale(0.25, 1);
+					}
+				}
+
+				.project-version {
+					color: rgb(var(--soda-less-white));
+				}
+			}
+
+			.project-body {
+				.project-description {
+					padding: 5px 0px;
+				}
+				
+				.project-tags {
+					display: flex;
+					flex-wrap: wrap;
+					gap: 10px 10px;
+					padding: 5px 0px;
+
+					.project-tag {
+						background-color: rgb(var(--soda-less-black));
+						border-radius: 5px;
+						padding: 3px 5px;
+					}
+				}
+			}
+		}
+
+		#project-none {
+			// font-size: 20px;
+			// line-height: 25px;
+			text-align: center;
+			font-weight: bold;
+		}
+	}
+
+	footer {
+		padding: 50px 0px;
+
+		#counter, #timelapse {
+			text-align: right;
+		}
 	}
 </style>
---
+++ src/routes/projects/soda/+page.svelte
@@ -0,0 +1,7 @@
+<div>
+	<p>MAMA MIA</p>
+	<p>PAPA PIA</p>
+	<p>THE BABY'S GOT THE DIARRHEA</p>
+
+	<p>Soda™ - Just the thing I need</p>
+</div>
